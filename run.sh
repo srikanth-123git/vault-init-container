5cc3d3c84254f94e61c892925a2a60901b952a6b
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-07-09 18:39:38 +0530

    Checking

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 vault login $VAULT_TOKEN &>/dev/null
-vault  kv get $SECRET_NAME  | sed -n -e '/= Data =/,$ p' | grep -Ev '= Data =|^Key|^--' | awk '{print "export "$1"="$2}'
+vault  kv get $SECRET_NAME  | sed -n -e '/= Data =/,$ p' | grep -Ev '= Data =|^Key|^--' | awk '{print "export "$1"="$2}' /data/secrets
 
 
