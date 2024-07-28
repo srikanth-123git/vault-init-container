efed7fa1ed8cd47a7433b61a323e7c467e74339b
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-07-28 23:25:04 +0530

    Vault init container

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 vault login $VAULT_TOKEN &>/dev/null
-vault  kv get $SECRET_NAME  | sed -n -e '/= Data =/,$ p' | grep -Ev '= Data =|^Key|^--' | awk '{print "export "$1"=\""$2"\""}' >/data/secrets
+vault  kv get $SECRET_NAME  | sed -n -e '/= Data =/,$ p' | grep -Ev '= Data =|^Key|^--' | awk '{print "export "$1"=\"$2"\""}' >/data/secrets
 cat /data/secrets
